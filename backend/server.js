13189797a4743e97d0cf6d017203aa303a28809f
joshua-z-zheng <jzheng796@gmail.com>
2025-01-25 03:50:00 -0500

    changed default localhost to 3001

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use('/auths', authRoutes);
 app.use('/users', userRoutes);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3001
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
